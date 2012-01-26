e6ae47dad6808344b2bfbffc824caa07d9a900c9
gustavogt <gustavogt@gmail.com>
2012-01-26 11:22:40 -0200

    Resolvendo o exercicio 2

--- tests/br/com/caelum/tdd/exercicio2/GeradorDeNotaFiscalTests.java
+++ tests/br/com/caelum/tdd/exercicio2/GeradorDeNotaFiscalTests.java
@@ -1,24 +1,27 @@
 package br.com.caelum.tdd.exercicio2;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
 public class GeradorDeNotaFiscalTests {
 
 	private static final double PRECISAO = 0.00001;
 	private EnviadorDeEmail email;
 	private NotaFiscalDao dao;
+	private Sap sap;
 	private GeradorDeNotaFiscal gerador;
 
 	@Before
 	public void setUp() {
 		email = mock(EnviadorDeEmail.class);
 		dao = mock(NotaFiscalDao.class);
+		sap = mock(SapComunicador.class);
 
-		gerador = new GeradorDeNotaFiscal(email, dao);
+		gerador = new GeradorDeNotaFiscal(email, dao, sap);
 	}
 	
 	@Test
@@ -46,5 +49,14 @@ public class GeradorDeNotaFiscalTests {
 		NotaFiscal nf = gerador.gera(fatura);
 		
 		verify(dao).persiste(nf);
+	}
+	
+	@Test
+	public void deveEnviarComunicacaoAoSap() {
+		Fatura fatura = new Fatura(1000, "cliente 1");
+		
+		NotaFiscal nf = gerador.gera(fatura);
+		
+		verify(sap).envia(nf);
 	}
 }
